795bcde5917812679068ba1640aa776859a69fe4
Julian Kamphausen <juliankamphausen@me.com>
2021-03-16 18:15:12 +0100

    cloudinary video 16/70

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,7 +1,16 @@
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData, statelessSessions
+} from '@keystone-next/keystone/session'
+
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 import { User } from './schemas/User';
 
 import 'dotenv/config';
-import { config, createSchema } from '@keystone-next/keystone/schema';
+
+
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -10,7 +19,17 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET
 }
 
-export default config({
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password']
+    // TODO: Add in initial roles here
+  }
+});
+
+export default withAuth(config({
   server: {
     cors: {
       origin: [process.env.FRONTEND_URL],
@@ -24,11 +43,18 @@ export default config({
   },
   lists: createSchema({
     // Schema items go in here
-    User
+    User,
+    Product,
+    ProductImage,
   }),
   ui: {
-    // TODO: change this for roles
-    isAccessAllowed: () => true,
+    // Show the UI only for people who pass this test
+    isAccessAllowed: ({ session }) => {
+      // console.log(session);
+      return !!session?.data;
+    },
   },
-  // TODO: Add session values here
-});
+  session: withItemData(statelessSessions(sessionConfig), {
+    User: `id`
+  })
+}));
